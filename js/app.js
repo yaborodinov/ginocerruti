490abd9225123859503c07e668c19abf7993a968
Alexandr <yaborodinov@gmail.com>
2021-02-17 21:54:10 +0200

    add modal size chart, add slider

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 "use strict"
 
+$(document).ready(function () {
+    $('.slider').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+});
+
 let scrollToTop = document.querySelector(".to_top");
 let header = document.querySelector(".header")
 
@@ -20,3 +29,58 @@ scrollToTop.addEventListener("click", () => {
 intoVideoBnt.addEventListener("click", () => {
     scrollDown(videoContent);
 })
+
+
+// change color
+
+let greenBtn = document.querySelector(".green");
+let burgundyBtn = document.querySelector(".burgundy");
+let midnight_blueBtn = document.querySelector(".midnight_blue");
+
+let background = document.querySelector(".main_photo");
+
+
+greenBtn.addEventListener("click", () => {
+    background.classList.add("green")
+    background.classList.remove("burgundy","midnight_blue");
+})
+
+
+burgundyBtn.addEventListener("click", () => {
+    background.classList.add("burgundy");
+    background.classList.remove("green","midnight_blue");
+})
+
+midnight_blueBtn.addEventListener("click", () => {
+    background.classList.add("midnight_blue")
+    ackground.classList.remove("green","burgundy");
+
+})
+
+
+// modal size_chart
+
+let modal = document.querySelector(".modal_wrapper");
+let btnOpen = document.querySelector(".product__size_chart_wrapper");
+let modalInner = document.querySelector(".modal__size_chart")
+
+
+btnOpen.addEventListener("click", (e) => {
+    
+    let y = e.clientY;
+    let x = e.clientX;
+    console.log(y)
+
+    modalInner.style.marginLeft = (x-593)+"px";
+    modalInner.style.marginTop = y+ "px";
+    modal.style.backgroundColor = "rgba(80, 77, 77, 0.418)"
+    modal.classList.add("modal_wrapper_show")
+
+})
+
+modal.addEventListener("click", function (e) {
+    if (e.target ===modal ){
+        modal.classList.remove("modal_wrapper_show")
+    }
+})
+
