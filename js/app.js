152576fe0abcfa46995edda8078fb8822da6c968
Alexandr <yaborodinov@gmail.com>
2021-02-17 16:43:35 +0200

    completed footer

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
 "use strict"
 
-let li = document.querySelectorAll(".nav__menu>li");
+let scrollToTop = document.querySelector(".to_top");
+let header = document.querySelector(".header")
 
-// let computedWidth = getComputedStyle(li)
+let intoVideoBnt = document.querySelector(".product__view_video");
+let videoContent = document.querySelector(".video")
+
+function scrollDown(el) {
+    el.scrollIntoView({ block: "start", behavior: "smooth" })
+}
 
 
 
-console.log(li[0])
 
+scrollToTop.addEventListener("click", () => {
+    scrollDown(header);
+})
+
+intoVideoBnt.addEventListener("click", () => {
+    scrollDown(videoContent);
+})
